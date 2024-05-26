554fb08bce4b7c7c13b4e97794e7620566ffc13a
Mehmet Topal <poseidon07mt@gmail.com>
2024-05-26 23:02:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+api çalışması için öncelikle proje içine node moduls yüklenmeli
+1- proje içinde terminal açım 
+2- npm install  komutunu girin
+node moduls yüklendi
+
+
+proje içinde .evn_example dosyasına girin 
+
+# API_KEY='api_key_here'
+# update file name .env
+
+dosyanın içi bu şekide 
+1-  .evn_example dosyasının ismini .evn olarak güncelleyin
+2-  ilk satırı yorumdan kaldırın , yani # silin 
+3-  raporda belirttiğim api key i yerine yazın
+4-  ikinci satır isteğe bağlı silinebilir silmicekseniz yorum halinde kalmalı 
+
+
+
+bu iki işlem tamamlandıktan sonra çalışmaya hazır durumda çalıstırmak için :
+1- proje klosörünün içinde terminal açın 
+2- ls komutunu yazın json dosyaları gözüküyor ise doğru yerdesiniz
+3- npm start  komutunu yazın 
+4- >  api-backend@1.0.0 start
+>  node Server.js
+
+ server is running: http://localhost:3000
+
+ terminal den bu şekilde cevap gelmiş ise burdaki işiniz bitti 
+ terminal açık kalsın 
+
+chatbot proje klosörüne geçebilirsiniz :)
---
+++ Server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const cors = require('cors'); 
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
+const app = express();
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
+require('dotenv').config();
+const api =process.env.API_KEY;
+const genAI = new GoogleGenerativeAI(api);
+
+
+
+
+
+app.get('/',  (req, res) => {
+    res.status(200).json({ message: "Ana sayfa Basarılı" });
+});
+
+
+
+
+app.post('/sendMsg', async (req, res) => {
+  const { message, history } = req.body;
+console.log(history);
+console.log(message);
+
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+  
+  
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+  
+  
+  
+  
+  
+  const chat = model.startChat({
+    history:history,
+    generationConfig,
+    safetySettings,
+
+  });
+
+  
+
+try {
+  
+  const result = await chat.sendMessage(message);
+  const response = await result.response;
+  const text = response.text();
+  return res.send(text);
+
+} catch (error) {
+  console.log(error);
+}
+
+
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is running: http://localhost:${PORT}`);
+});
