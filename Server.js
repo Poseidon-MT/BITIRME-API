b456f829e81c754b07e6e070f7b09e32620b47d7
Mehmet Topal <poseidon07mt@gmail.com>
2025-04-15 14:59:07 +0300

    update api model

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
 const app = express();
 app.use(cors({
@@ -7,15 +7,15 @@ app.use(cors({
 }));
 app.use(express.json());
 require('dotenv').config();
-const api =process.env.API_KEY;
+const api = process.env.API_KEY;
 const genAI = new GoogleGenerativeAI(api);
 
 
 
 
 
-app.get('/',  (req, res) => {
-    res.status(200).json({ message: "Ana sayfa Basarılı" });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Ana sayfa Basarılı" });
 });
 
 
@@ -23,12 +23,12 @@ app.get('/',  (req, res) => {
 
 app.post('/sendMsg', async (req, res) => {
   const { message, history } = req.body;
-console.log(history);
-console.log(message);
+  console.log(history);
+  console.log(message);
 
-  const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-  
-  
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+
+
   const generationConfig = {
     temperature: 0.9,
     topK: 1,
@@ -42,30 +42,30 @@ console.log(message);
       threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
     },
   ];
-  
-  
-  
-  
-  
+
+
+
+
+
   const chat = model.startChat({
-    history:history,
+    history: history,
     generationConfig,
     safetySettings,
 
   });
 
-  
 
-try {
-  
-  const result = await chat.sendMessage(message);
-  const response = await result.response;
-  const text = response.text();
-  return res.send(text);
 
-} catch (error) {
-  console.log(error);
-}
+  try {
+
+    const result = await chat.sendMessage(message);
+    const response = await result.response;
+    const text = response.text();
+    return res.send(text);
+
+  } catch (error) {
+    console.log(error);
+  }
 
 
 });
